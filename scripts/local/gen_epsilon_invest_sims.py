bf26770a2f9d183549b8825c54ba00e203c382c2
Ian Graham <irgraham1@gmail.com>
2021-09-26 13:18:46 -0400

    new rrlogin scripts

--- scripts/local/gen_epsilon_invest_sims.py
+++ scripts/local/gen_epsilon_invest_sims.py
@@ -1,25 +1,26 @@
 import glob
 import subprocess
 import numpy as np
+import sys
 
-As = np.linspace(0.1, 1.0, 10)
+As = np.linspace(0.1, 0.3, 15)
 as_str = ",".join([str(a) for a in As])
 
-temps = np.logspace(0, -4, 41)
+temps = np.logspace(0, 1, 11)
 
-files = glob.glob("/home/ian/Documents/Data/MD_LV_investigate/equil/equil_*hertz*")
+files = glob.glob("/home/ian/Documents/Data/MD_LV_investigate/equil/equil_*na-5*seed-1*hertz*")
 
-idx = 200
+idx = 500
 
 for i, f in enumerate(files):
     print(i)
     for temp in temps:
         print(temp)
 
-        command = f'/home/ian/Documents/Projects/md-lv/target/release/md-lv --unwrap --potential lj --time 100.0 \
-            --init-config {f} --temp {temp} --seed {idx} --dt 1e-2 --out-time 1e-1 \
-            --dir /home/ian/Documents/Data/MD_LV_investigate/temp_hertz \
-            gen-variant --realizations 10 --del-var=0.0 --calc-msd --calc-pos --calc-q={as_str}'
+        command = f'/home/ian/Documents/Projects/md-lv/target/release/md-lv --unwrap --potential hertz --time 100.0 \
+            --init-config {f} --vscale {temp} --seed {idx} --dt 1e-3 --out-time 1e-2 \
+            --dir /home/ian/Documents/Data/MD_LV_investigate/epsilon_hertz \
+            gen-variant --realizations 100 --del-var=0.0 --calc-msd --calc-pos --calc-q={as_str}'
 
         input = command.split()
         subprocess.run(input)
@@ -34,9 +35,9 @@ for i, f in enumerate(files):
         print(temp)
 
         command = f'/home/ian/Documents/Projects/md-lv/target/release/md-lv --unwrap --potential lj --time 100.0 \
-            --init-config {f} --temp {temp} --seed {idx} --dt 1e-3 --out-time 1e-2 \
-            --dir /home/ian/Documents/Data/MD_LV_investigate/temp_lj \
-            gen-variant --realizations 10 --del-var=0.0 --calc-msd --calc-pos --calc-q={as_str}'
+            --init-config {f} --vscale {temp} --seed {idx} --dt 1e-3 --out-time 1e-2 \
+            --dir /home/ian/Documents/Data/MD_LV_investigate/epsilon_lj \
+            gen-variant --realizations 100 --del-var=0.0 --calc-msd --calc-pos --calc-q={as_str}'
 
         input = command.split()
         subprocess.run(input)
