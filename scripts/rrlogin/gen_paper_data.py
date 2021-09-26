18908961af60caf1b0995f52abd9f49dec274301
Ian Graham <igraham1@gmail.com>
2021-09-26 13:38:03 -0400

    fixed script

--- scripts/rrlogin/gen_paper_data.py
+++ scripts/rrlogin/gen_paper_data.py
@@ -10,21 +10,21 @@ as_str = ",".join([str(a) for a in As])
 vs = np.logspace(0, 1, 11)
 vs_str = ",".join([str(a) for a in vs])
 
-idx = int(sys.argv[1]) -1
+idx = int(sys.argv[1]) + 999
 
 if idx%2 == 0:
     pot = "hertz"
 else:
     pot = "lj"
 
-files = glob.glodb("/home/igraham/data/MD_LV_paper_data/equil/equil_*{pot}*")
+files = glob.glob(f"/home/igraham/data/MD_LV_paper_data/equil/equil_*{pot}*")
 
 assert(len(files) == 1)
 f = files[0]
 
-command = f'/home/ian/Documents/Projects/md-lv/target/release/md-lv --unwrap --potential {pot} \
+command = f'/home/igraham/Documents/md-lv/target/release/md-lv --unwrap --potential {pot} \
     --init-config {f} --temp 0.1 --seed {idx} --dt 1e-3 --out-time 1e-2 \
-    --dir /home/igraham/data/MD_LV_paper_data/{pot}_pred \
+    --dir /home/igraham/data/MD_LV_paper_data/{pot}_pred --time 100 \
     gen-variant --realizations 1000 --del-var={vs_str} --calc-msd --calc-pos --calc-q={as_str}'
 
 input = command.split()
